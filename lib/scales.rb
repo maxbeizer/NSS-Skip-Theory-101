ea1eee15b93e1559e15882e3907f79e7bbed302e
maxbeizer <max.beizer@gmail.com>
2012-10-09 15:36:32 -0500

    refactored out the case, but must add mappings hash

--- lib/scales.rb
+++ lib/scales.rb
@@ -10,43 +10,8 @@ class Scales
 #move this to its own super class since scales and chords will need start 
 #position, no?
   def start_position
-  	case @scale_name
-  	when /^c[mc]/i
-  	  SHARPS[0]
-	when /^c#[mc]/i
-  	  SHARPS[1]
-   	when /^db[mc]/i
-  	  FLATS[1]
-  	when /^d[mc]/i
-  	  SHARPS[2]
-  	when /^d#[mc]/i
-  	  SHARPS[3]
-   	when /^eb[mc]/i
-  	  FLATS[3]  	
-  	when /^e[mc]/i
-  	  SHARPS[4]
-  	when /^f[mc]/i
-  	  SHARPS[5]  	
-  	when /^f#[mc]/i
-  	  SHARPS[6]
-   	when /^gb[mc]/i
-  	  FLATS[6]
-  	when /^g[mc]/i
-  	  SHARPS[7]  	
-  	when /^g#[mc]/i
-  	  SHARPS[8]
-   	when /^ab[mc]/i
-  	  FLATS[8]  
-  	when /^a[mc]/i
-  	  SHARPS[9]  	
-  	when /^a#[mc]/i
-  	  SHARPS[10]
-   	when /^bb[mc]/i
-  	  FLATS[10]  
-  	when /^b[mc]/i
-  	  SHARPS[11]
-  	else
-  	  raise ArgumentError, "invalid chord name" 
-  	end
+ 	@scale_name =~ /(\w|(\w[b#]))(m|cr)/i
+ 	$1.capitalize
+ 	#MAPPINGS($1)
   end
 end #class Scales
