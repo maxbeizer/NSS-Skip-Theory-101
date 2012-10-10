90879fadea02aada72f4b1d350af0304a00b6854
maxbeizer <max.beizer@gmail.com>
2012-10-10 09:24:10 -0500

    added more pseudo constants to test for greater range

--- test/test_scales_major.rb
+++ test/test_scales_major.rb
@@ -2,8 +2,16 @@ require 'test/unit'
 require 'scales_major'
 
 class Scales_MajorTest < Test::Unit::TestCase
-  def example_scale
-  	ScalesMajor.new("cmaj")
+  def example_scale1
+   	Scales.new("cmaj")
+  end 
+
+  def example_scale2
+  	Scales.new("dmaj")
+  end
+
+  def example_scale3
+  	Scales.new("dbmaj")
   end
 
   def test_01_inherits_from_scales
