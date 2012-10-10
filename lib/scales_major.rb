38b96f7509bb1cc89347fffda86792ab025893ce
maxbeizer <max.beizer@gmail.com>
2012-10-10 09:44:03 -0500

    added start index method

--- lib/scales_major.rb
+++ lib/scales_major.rb
@@ -1,5 +1,13 @@
 class ScalesMajor < Scales
   MAXIMUM_NOTES = 7
 
+  def start_index
+  	SHARPS.index(start_position)
+  end
+
+  def build_major_scale
+  	major_scale = []
+  	major_scale << start_position
+  end
 
 end
