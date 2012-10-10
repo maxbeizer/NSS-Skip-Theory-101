d0a1dd7f466098a149aacca99e98d216f48736ef
maxbeizer <max.beizer@gmail.com>
2012-10-10 11:30:40 -0500

    added and modified scales minor sharps and flats

--- lib/scales_minor_flats.rb
+++ lib/scales_minor_flats.rb
@@ -1,19 +1,19 @@
 require 'scales_minor'
 
-class ScalesMinorFlats < ScalesMajor
+class ScalesMinorFlats < ScalesMinor
   def start_index
   	FLATS.index(start_position)
   end
 
-  def build_minor_scale
+  def build_minor_scale_flats
   	major_scale = []
   	major_scale << start_position
   	major_scale << FLATS[start_index+2]
-  	major_scale << FLATS[start_index+4]
+  	major_scale << FLATS[start_index+3]
   	major_scale << FLATS[start_index+5]
   	major_scale << FLATS[start_index+7]
-  	major_scale << FLATS[start_index+9]
-  	major_scale << FLATS[start_index+11]
+  	major_scale << FLATS[start_index+8]
+  	major_scale << FLATS[start_index+10]
   	major_scale
   end
 end
--- lib/scales_minor_sharps.rb
+++ lib/scales_minor_sharps.rb
@@ -1,19 +1,19 @@
 require 'scales_minor'
 
-class ScalesMinorSharps < ScalesMajor
+class ScalesMinorSharps < ScalesMinor
   def start_index
   	SHARPS.index(start_position)
   end
 
-  def build_minor_scale
+  def build_minor_scale_sharps
   	major_scale = []
   	major_scale << start_position
   	major_scale << SHARPS[start_index+2]
-  	major_scale << SHARPS[start_index+4]
+  	major_scale << SHARPS[start_index+3]
   	major_scale << SHARPS[start_index+5]
   	major_scale << SHARPS[start_index+7]
-  	major_scale << SHARPS[start_index+9]
-  	major_scale << SHARPS[start_index+11]
+  	major_scale << SHARPS[start_index+8]
+  	major_scale << SHARPS[start_index+10]
   	major_scale
   end
 end
