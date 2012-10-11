cece27e9e1c88205a466196804920fa2e391f1a6
maxbeizer <max.beizer@gmail.com>
2012-10-11 11:02:23 -0500

    added chord question to basic skip theory file

--- lib/skip_music_theory.rb
+++ lib/skip_music_theory.rb
@@ -1,10 +1,22 @@
-require_relative "scales"
 require_relative "scales_major_sharps"
 require_relative "scales_minor_sharps"
+require_relative "scales_major_flats"
+require_relative "scales_minor_flats"
+require_relative "chords_major_sharps"
+require_relative "chords_minor_sharps"
+require_relative "chords_major_flats"
+require_relative "chords_minor_flats"
 
-print "Enter a major scale: "
-entry = gets.chomp
+print "Enter a scale: "
+scale = gets.chomp
 
-answer = Scales.new(entry).generate_scale
+scale_answer = Scales.new(scale).generate_scale
 
-puts answer
+puts scale_answer
+
+print "Enter a chord: "
+chord = gets.chomp
+
+chord_answer = Chords.new(chord).generate_chord
+
+puts chord_answer
