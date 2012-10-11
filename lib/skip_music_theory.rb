a1659b88246e45b96157fa4248e68196f7abd421
maxbeizer <max.beizer@gmail.com>
2012-10-11 12:45:07 -0500

    added class methods to learning class

--- lib/skip_music_theory.rb
+++ lib/skip_music_theory.rb
@@ -1,10 +1,31 @@
-print "What would you like to learn about? Chords or Scales?  "
-first_response = gets.downcase.chomp!
+require_relative "scales_major_sharps"
+require_relative "scales_minor_sharps"
+require_relative "scales_major_flats"
+require_relative "scales_minor_flats"
+require_relative "chords_major_sharps"
+require_relative "chords_minor_sharps"
+require_relative "chords_major_flats"
+require_relative "chords_minor_flats"
 
-if first_response == "chords"
-  #open chords learning file
-elsif first_response == "scales"
-  #opend scales learning file
-else
-  puts "Invalid input, stoop. Run the program again."
+class Learning
+  def self.process
+    print "What would you like to learn about? Chords or Scales?  "
+    first_response = gets.downcase.chomp!
+
+    if first_response == "chords"
+      learning_chords
+    elsif first_response == "scales"
+      #opend scales learning file
+    else
+      puts "Invalid input, stoop. Run the program again."
+    end
+  end
+
+  def self.learning_chords
+    print "Enter a chord: "
+    chord = gets.chomp
+    chord_answer = Chords.new(chord).generate_chord
+    puts chord_answer	
+  end
+  Learning.process
 end
