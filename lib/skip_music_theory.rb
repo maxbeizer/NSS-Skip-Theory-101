60393d271124a312671bac57b12d1ce14edf3503
maxbeizer <max.beizer@gmail.com>
2012-10-11 13:04:17 -0500

    added rerun method to class so that the program will keep running until you say stop

--- lib/skip_music_theory.rb
+++ lib/skip_music_theory.rb
@@ -11,21 +11,35 @@ class Learning
   def self.process
     print "What would you like to learn about? Chords or Scales?  "
     first_response = gets.downcase.chomp!
-
     if first_response == "chords"
       learning_chords
     elsif first_response == "scales"
-      #opend scales learning file
+    	learning_scales
     else
       puts "Invalid input, stoop. Run the program again."
     end
+  end
+
+  def self.rerun_or_exit
+  	print "Would you like to try some more? Yes or No: "
+  	rerun_response = gets.downcase.chomp!
+  	Learning.process if rerun_response == "yes"
   end
 
   def self.learning_chords
     print "Enter a chord: "
     chord = gets.chomp
     chord_answer = Chords.new(chord).generate_chord
-    puts chord_answer	
+    puts chord_answer
+		rerun_or_exit	
+  end
+
+  def self.learning_scales
+		print "Enter a scale: "
+		scale = gets.chomp
+		scale_answer = Scales.new(scale).generate_scale
+		puts scale_answer
+		rerun_or_exit  	
   end
   Learning.process
 end
