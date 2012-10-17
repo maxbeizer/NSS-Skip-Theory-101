a8618394e8b3a075e9550240795efd094f54ecc4
maxbeizer <max.beizer@gmail.com>
2012-10-17 14:05:06 -0500

    refactored out separate maj/min options for chords and scales, now working on refactoring options for chords and scales

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -92,9 +92,9 @@ class Quiz
     print "Which would you like to get quizzed on: major chords or minor chords?  "
     options_response = clean_gets
     if options_response =~ /^major/
-      quiz_chords_major
+      quiz_chords("maj")
     elsif options_response =~ /^minor/
-      quiz_chords_minor
+      quiz_chords("min")
     elsif options_response =~ /(exit)|(^q)/ 
       exit
     else
@@ -103,11 +103,11 @@ class Quiz
     end
   end
 
-  def quiz_chords_major
+  def quiz_chords(type)
     quiz_chord_start = @@path_array.sample
     index = @@path_array.index(quiz_chord_start)
-    generated_quiz_chord = Chords.new(quiz_chord_start + "maj").generate_chord
-    print "Please enter a " + quiz_chord_start + "maj chord (case insensitive):  "
+    generated_quiz_chord = Chords.new(quiz_chord_start + type).generate_chord
+    print "Please enter a " + quiz_chord_start + type + " chord (case insensitive):  "
     quiz_chord_answer = clean_gets
     puts "You entererd: " + quiz_chord_answer
     puts "The correct answer was: " + generated_quiz_chord
@@ -125,35 +125,28 @@ class Quiz
     end
   end
 
-  def quiz_chords_minor
-    quiz_chord_start = @@path_array.sample
-    index = @@path_array.index(quiz_chord_start)
-    generated_quiz_chord = Chords.new(quiz_chord_start + "min").generate_chord
-    print "Please enter a " + quiz_chord_start + "min chord (case insensitive):  "
-    quiz_chord_answer = clean_gets
-    puts "You entererd: " + quiz_chord_answer
-    puts "The correct answer was: " + generated_quiz_chord
-    if quiz_chord_answer.downcase == generated_quiz_chord.downcase
-      puts "You got it correct. You rock my socks!"
-      generate_pass
-      move_down_the_path(index)
-      display_path_progress
-      rerun_or_exit(:quiz_chords_options)  
+  def quiz_options(scales_or_chords)
+   print "Which would you like to get quizzed on: major " + scales_or_chords + " or minor " + scales_or_chords + "?  "
+    options_response = clean_gets
+    if options_response =~ /^major/
+      quiz_scales("maj")
+    elsif options_response =~ /^minor/
+      quiz_scales("min")
+    elsif options_response =~ /(exit)|(^q)/ 
+      exit
     else
-      puts "You were so close!"
-      generate_fail
-      display_path_progress
-      rerun_or_exit(:quiz_chords_options)      
-    end
+      puts "I didn't quite understand that; try again please."
+      quiz_options(scales_or_chords)
+    end  
   end
 
   def quiz_scales_options
     print "Which would you like to get quizzed on: major scales or minor scales?  "
     options_response = clean_gets
     if options_response =~ /^major/
-      quiz_scales_major
+      quiz_scales("maj")
     elsif options_response =~ /^minor/
-      quiz_scales_minor
+      quiz_scales("min")
     elsif options_response =~ /(exit)|(^q)/ 
       exit
     else
@@ -162,40 +155,27 @@ class Quiz
     end  	
   end
 
-  def quiz_scales_major
-    quiz_scale_start = ALL_NOTES.sample
-    generated_quiz_scale = Scales.new(quiz_scale_start + "maj").generate_scale
-    print "Please enter a " + quiz_scale_start + "maj scale (case insensitive):  "
+  def quiz_scales(type)
+    quiz_scale_start = @@path_array.sample
+    index = @@path_array.index(quiz_scale_start)
+    generated_quiz_scale = Scales.new(quiz_scale_start + type).generate_scale
+    print "Please enter a " + quiz_scale_start + type + " scale (case insensitive):  "
     quiz_scale_answer = clean_gets
     puts "You entererd: " + quiz_scale_answer
     puts "The correct answer was: " + generated_quiz_scale
     if quiz_scale_answer.downcase == generated_quiz_scale.downcase
       puts "You got it correct. You rock my socks!"
       generate_pass
+      move_down_the_path(index)
+      display_path_progress
       rerun_or_exit(:quiz_scales_options) 
     else
       puts "You were so close!"
       generate_fail
+      display_path_progress
       rerun_or_exit(:quiz_scales_options)     
     end
   end
 
-  def quiz_scales_minor
-    quiz_scale_start = ALL_NOTES.sample
-    generated_quiz_scale = Scales.new(quiz_scale_start + "min").generate_scale
-    print "Please enter a " + quiz_scale_start + "min scale (case insensitive):  "
-    quiz_scale_answer = clean_gets
-    puts "You entererd: " + quiz_scale_answer
-    puts "The correct answer was: " + generated_quiz_scale
-    if quiz_scale_answer.downcase == generated_quiz_scale.downcase
-      puts "You got it correct. You rock my socks!"
-      generate_pass
-      rerun_or_exit(:quiz_scales_options) 
-    else
-      puts "You were so close!"
-      generate_fail
-      rerun_or_exit(:quiz_scales_options)     
-    end
-  end
   Quiz.new().process
 end
